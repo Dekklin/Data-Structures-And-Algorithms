023c3845798225001b1d393d41751f431ba6fda5
Dekklin <collinhintzke@outlook.com>
2018-06-11 16:37:29 -0700

    Update Program.cs

--- Challenges/02BinarySearch/02BinarySearch/Program.cs
+++ Challenges/02BinarySearch/02BinarySearch/Program.cs
@@ -4,6 +4,7 @@ namespace _02BinarySearch
 {
     class Program
     {
+        //method that defines a test array and a test search key
         static void Main(string[] args)
         {
             int[] testArray = new int[] { 1, 3, 4, 5, 6, 8};
@@ -12,8 +13,9 @@ namespace _02BinarySearch
             Print(testArray);
             Console.WriteLine($"Your Search Key was {SK}");
             Console.Read();
-
         }
+        //Method that cuts an array in half, if the search key is less than that middle point, it makes that mid point the new max, and runs the method again.
+        //if the mid point was less than the SK, it makes the mid point the new min point and runs the method again with the new arguements
         static int BinarySearch(int[] array, int SK,int Max, int Min)
         {
             int results = -1;
@@ -36,6 +38,7 @@ namespace _02BinarySearch
 
             return results;
         }
+        //This method simply prints out our array for testing and visibility reasons. 
         static void Print(int[] array)
         {
             foreach(int i in array)
