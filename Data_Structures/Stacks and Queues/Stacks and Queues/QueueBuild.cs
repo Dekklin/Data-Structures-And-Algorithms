699c9fddc53ab5061fa365ba1dc50b3233379906
Collin Hintzke <collinhintzke5@gmail.com>
2018-06-18 11:44:06 -0700

    Coding done

---
+++ Data_Structures/Stacks and Queues/Stacks and Queues/QueueBuild.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks_and_Queues
+{
+    public class QueueBuild
+    {
+        public Node Rear { get; set; }
+        public Node Front { get; set; }
+
+        // queue constructor
+        public QueueBuild(Node node)
+        {
+            Front = node;
+            Rear = node;
+        }
+        // Adds a node to the queue
+        public Node EnQueue(Node node)
+        {
+            Rear.Next = node;
+            Rear = node;
+            return Rear;
+        }
+        // removes the next in line Node from the queue
+        public Node DeQueue()
+        {
+            Node temp = Front;
+            Front = Front.Next;
+            temp.Next = null;
+            return temp;
+        }
+        // returns the next in line node from the queue
+        public Node Peek()
+        {
+            return Front;
+        }
+        // prints all the nodes in the queue
+        public void Print()
+        {
+            Node Current = Front;
+            while (Current.Next != null)
+            {
+                Console.WriteLine(Current.Value);
+                Current = Current.Next;
+            }
+            Console.WriteLine(Current.Value);
+        }
+    }
+}
