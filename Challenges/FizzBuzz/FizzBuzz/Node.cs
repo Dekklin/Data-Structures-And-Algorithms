1ddcdb161d5ad19673bfc87d023a66f5e085cdc7
Collin Hintzke <collinhintzke5@gmail.com>
2018-06-26 19:24:39 -0700

    Finished

---
+++ Challenges/FizzBuzz/FizzBuzz/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTree
+{
+    public class Node
+    {
+        public Node Next { get; internal set; }
+        public string Value { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+
+        public Node(string value)
+        {
+            Value = value;
+        }
+}
+}
