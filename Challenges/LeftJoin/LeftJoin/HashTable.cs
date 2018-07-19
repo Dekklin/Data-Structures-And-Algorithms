f30b78d9bafb3469dfcd00c53b7381ad36768343
Collin Hintzke <collinhintzke5@gmail.com>
2018-07-19 15:24:15 -0700

    finished

---
+++ Challenges/LeftJoin/LeftJoin/HashTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeftJoin
+{
+    public class HashTable
+    {
+        public string[] table { get; set; }
+        public List<string> keys { get; set; }
+
+        public HashTable()
+        {
+            table = new string[1024];
+            keys = new List<string>();
+
+        }
+        /// <summary>
+        /// Takes a Key and a Value, both strings in this instance. 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string Add(string key, string value)
+        {
+            keys.Add(key);
+            int index = Hash(key);
+            if(table[index] == null)
+            {
+                return table[index] = value;
+            }
+            return "null";
+        }
+        /// <summary>
+        /// Grabs the hash'd value of the string I send it
+        /// </summary>
+        /// <param name="key">Which I grab the ASCII values and add them together, do math, and send it to my Add Method</param>
+        /// <returns>And integer, which represents the index of that key</returns>
+        public int Hash(string key)
+        {
+            int finalValue = 0;
+            byte[] ascii = Encoding.ASCII.GetBytes(key);
+            foreach (byte b in ascii)
+            {
+                finalValue += Convert.ToInt32(b);
+            }
+            finalValue = finalValue * 1024;
+            finalValue = finalValue * key.Length;
+            finalValue = finalValue / 1001 * key.Length;
+            finalValue = finalValue % 1024;
+            return finalValue;
+        }
+    }
+}
---
+++ Challenges/LeftJoin/LeftJoin/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeftJoin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable hs = new HashTable();
+            hs.Add("fond", "enamored");
+            hs.Add("wrath", "anger");
+            hs.Add("diligent", "employed");
+            hs.Add("outfit", "garb");
+            hs.Add("guide", "usher");
+
+
+            HashTable hs2 = new HashTable();
+            hs2.Add("fond", "adverse");
+            hs2.Add("wrath", "delight");
+            hs2.Add("diligent", "idle");
+            hs2.Add("guide", "follow");
+            hs2.Add("flow", "jam");
+
+            JoinLeft(hs, hs2);
+            Console.ReadKey();
+
+        }
+        /// <summary>
+        /// JoinLeft takes two HashTables, and combines the second HS, and checks if there are any similar keys, if there are they add their values to a list, if theres a key in HashTable1, then we add Null at the end of the value and add it to the list
+        /// </summary>
+        /// <param name="hs">Hashtable 1, our base</param>
+        /// <param name="hs2">Hashtable 2, what we compare to Hashtable 1</param>
+        /// <returns>List of strings, a concatonated string of similar keys and their values</returns>
+        public static List<string> JoinLeft(HashTable hs, HashTable hs2)
+        {
+            List<string> datList = new List<string>();
+            foreach (string key in hs.keys)
+            {
+                datList.Add($"{key} : {hs.table[hs.Hash(key)]}");
+            }
+            foreach (string key in hs2.keys)
+            {
+                for (int i = 0; i < hs2.keys.Count; i++)
+                {
+                    if (key == hs.keys[i])
+                    {
+                        datList[i] = $"{datList[i]} & {hs2.table[hs2.Hash(key)]}";
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < hs.keys.Count; i++)
+            {
+                if (!hs2.keys.Contains(hs.keys[i]))
+                {
+                    datList[i] = $"{datList[i]} & Null";
+                }
+            }
+            return datList;
+        }
+    }
+}
